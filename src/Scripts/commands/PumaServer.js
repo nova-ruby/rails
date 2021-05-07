1192da9e9a5fa7c73613b75d79c7d8953e55f040
Tommaso Negri <tommasongr@icloud.com>
2021-05-07 20:26:52 +0000

    feat: close the stdio for the kill puma server command

--- src/Scripts/commands/PumaServer.js
+++ src/Scripts/commands/PumaServer.js
@@ -4,7 +4,7 @@ export default class PumaServer {
     kill() {
         const process = new Process("usr/bin/env", {
             args: ["pkill", "-9", "-f", "rb-fsevent|rails|spring|puma"],
-            stdio: ["ignore", "pipe", "ignore"],
+            stdio: ["ignore", "ignore", "ignore"],
         });
 
         process.start();
