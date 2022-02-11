d8f023c237161e6f4ce7e261def355c92e646fb3
Tommaso Negri <tommaso@hey.com>
2022-02-11 14:35:49 +0100

    chore: Remove prettier and change editor configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,8 @@
 module.exports = {
-    root: true,
-    env: {
-        commonjs: true,
-        "nova/nova": true,
-    },
-    extends: ["plugin:prettier/recommended"],
-    plugins: ["nova", "prettier"],
-    rules: {
-        "prettier/prettier": "error",
-    },
-};
+  root: true,
+  env: {
+    commonjs: true,
+    "nova/nova": true,
+  },
+  plugins: ["nova"],
+}
